489fa4583e869d9a23ad057e262f2f0a808aba13
GodRyosuke <ienvs0828@outlook.jp>
2022-03-13 08:02:51 +0900

    removed Debug and Release Folder

---
+++ wander_opengl/include/Texture.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include "glew.h"
+#include "SDL_ttf.h"
+#include "SDL.h"
+
+class Texture {
+public:
+	Texture();
+	Texture(std::string filePath);
+
+	void CreateTextTexture(std::string text_data, int* color, TTF_Font* font);
+
+	void BindTexture();
+	void UnBindTexture();
+
+	int getWidth() { return width; }
+	int getHeight() { return height; }
+
+private:
+	void createFromSurf(SDL_Surface* surf);
+
+	int width;
+	int height;
+	GLuint texture_data;
+};
